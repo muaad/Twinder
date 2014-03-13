275eaf07cfaab292fe73fb98355bd339eed0ebfd
muaad <muadh24@gmail.com>
2014-03-13 16:07:22 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twinder::Application.config.secret_key_base = '47fc7a9efa4a66f70e214bca04bc7f0671527acd692b0ab95b4c43e416136dd4ab4907e73aad4cc331032b77baee58d1f71b63956c302cd34992b00bfa086522'
